94b222a8a26c85b5e519fd7b03dcbf659c6dc19b
asdzxc3677 <yoha3677@naver.com>
2022-07-05 09:44:15 +0900

    페이징 처리 진행중

--- src/main/java/com/example/boot_review/config/WebConfig.java
+++ src/main/java/com/example/boot_review/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.example.boot_review.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private String connectPath = "/upload/**";
+    private String resourcePath = "file:///D:/springboot_img/";
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registr){
+        registr.addResourceHandler(connectPath)
+                .addResourceLocations(resourcePath);
+    }
 }
